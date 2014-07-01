2a16d06a7970de7887cebf9b5cf4335fae300ea2
Ondrej Kocian <ondrej.kocian@firma.seznam.cz>
2014-07-01 14:31:27 +0200

    using openjdk-7-jre

--- bin/jsdoc2.sh
+++ bin/jsdoc2.sh
@@ -7,6 +7,6 @@ echo "jsdoc_toolkit 2.x starting"
 echo
 
 #vlastni volani jsdoc
-/usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java -jar "$DOC_DIR/jsrun.jar" "$DOC_DIR/app/run.js" -t=/www/util/jsdoc2/templates/szn/ -d=out -v $*
+/usr/lib/jvm/java-7-openjdk-amd64/jre/bin/java -jar "$DOC_DIR/jsrun.jar" "$DOC_DIR/app/run.js" -t=/www/util/jsdoc2/templates/szn/ -d=out -v $*
 
 echo
